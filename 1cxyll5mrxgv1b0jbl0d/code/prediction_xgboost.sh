3940ab20a76c013b8f866c2140a7608e76b92c0f
didarul_csiss <mislam25@gmu.edu>
2022-03-20 11:27:15 -0400

    shell

---
+++ 1cxyll5mrxgv1b0jbl0d/code/prediction_xgboost.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpuq                    # the DGX only belongs in the 'gpu'  partition
+#SBATCH --qos=gpu                           # need to select 'gpu' QoS
+#SBATCH --job-name=python-gpu
+#SBATCH --output=python-gpu.%j.out
+#SBATCH --error=python-gpu.%j.err
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=2                 # up to 128; 
+#SBATCH --gres=gpu:A100.40gb:1              # up to 8; only request what you need
+#SBATCH --mem-per-cpu=3500M                 # memory per CORE; total memory is 1 TB (1,000,000 MB)
+#SBATCH --export=ALL 
+#SBATCH --time=0-00:20:00                   # set to 1hr; please choose carefully
+
+set echo
+umask 0027
+
+
+python prediction_xgboost.py
